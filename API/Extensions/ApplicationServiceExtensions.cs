4fafd15c61616cb31432d50dc0ddd45dccb2b688
lostapplesauce <48301604+lostapplesauce@users.noreply.github.com>
2024-01-01 23:01:10 +0000

    Added Application folder and classes within

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using MediatR;
+using Application.Activities;
+using Application.Core;
+
+namespace API.Extensions
+{
+    //We don't what or need to create a new instance of this class
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+         IConfiguration config)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3001");
+                });
+            });
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
